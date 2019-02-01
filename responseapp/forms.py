d4e3e1e609feb23052f3c2212b4bd0f494b00cbc
neeraj sharma <neeraj@neerajs-MacBook-Air.local>
2019-02-01 17:10:04 +0530

    djangoApp selenium added

--- responseapp/forms.py
+++ responseapp/forms.py
@@ -1,7 +1,9 @@
 
 from django import forms
 
+
 class MyForm(forms.Form):
  name = forms.CharField(label='Enter your name', max_length=100)
  email = forms.EmailField(label='Enter your email', max_length=100)
- feedback = forms.CharField(widget=forms.Textarea(attrs={'width':"100%", 'cols' : "80", 'rows': "20", }))
+ password = forms.CharField(label='Enter your password', widget=forms.PasswordInput())
+ mobilenumber = forms.CharField(label='Enter your mobile number', max_length=10)
--- responseapp/views.py
+++ responseapp/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render
 from responseapp.forms import MyForm
 from django.template import loader
 from django.http import HttpResponse
-# Create your views here.
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common import action_chains
+from selenium.webdriver.chrome.options import Options
+import time
+import os
+
+baseUrl1 = "https://zerodha.com/open-account?c=ZMPTJT"
+
+WINDOW_SIZE = "1920,1080"
+
+chrome_options = Options()  
+# chrome_options.add_argument("--headless")  
+chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
 
 
 def responseform(request):
@@ -14,13 +27,44 @@ def responseform(request):
         if myForm.is_valid():
             name = myForm.cleaned_data['name']
             email = myForm.cleaned_data['email']
-            feedback = myForm.cleaned_data['feedback']
+            password = myForm.cleaned_data['password']
+            mobilenumber = myForm.cleaned_data['mobilenumber']
 
             context = {
             'name': name,
             'email': email,
-            'feedback': feedback
+            'password': password,
+            'mobilenumber': mobilenumber
             }
+
+            # for MAC
+            try:
+                CHROME_PATH = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+                PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+                DRIVER_BIN = os.path.join(PROJECT_ROOT, "chromedriver")
+                chrome_options.binary_location = CHROME_PATH
+                driver = webdriver.Chrome(executable_path = DRIVER_BIN, chrome_options = chrome_options)
+
+            # for windows
+            except:
+                print('error in mac')
+                # CHROME_PATH = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+                # chrome_options.binary_location = CHROME_PATH
+                # dir_path = os.getcwd() + '\chromedriver.exe'
+                # driver =  webdriver.Chrome(executable_path = dir_path, chrome_options = chrome_options)
+
+            driver.get(baseUrl1)
+            wname =  driver.find_element_by_name('user_name')
+            wmobile =  driver.find_element_by_name('user_mobile')
+            wemail = driver.find_element_by_name('user_email')
+            wname.clear()
+            wname.send_keys(name)
+            wmobile.clear()
+            wmobile.send_keys(mobilenumber)
+            wemail.clear()
+            wemail.send_keys(email)
+            openButton = driver.find_element_by_xpath('//*[@id="open_account_proceed_form"]')
+            openButton.click()
 
             template = loader.get_template('thankyou.html')
 
